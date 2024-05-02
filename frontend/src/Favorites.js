d66591e3064d2e90315b61b82ba97a47d2f4e4ba
Felicia Atterling <felicia.atterling@gmail.com>
2024-05-02 17:29:15 -0500

    Added favorites

--- frontend/src/Favorites.js
+++ frontend/src/Favorites.js
@@ -1,6 +1,128 @@
 // Component for favorite locations on home page
-function Favorites() {
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
+function Favorites() {
+  const [favs, setFavs] = useState([]);
+  const [favsView, setFavsView] = useState(0);
+
+  useEffect(() => {
+    fetchFavs();
+  }, []);
+
+  const fetchFavs = async () => {
+    try {
+      const response = await fetch("http://localhost:8082/listFavorites");
+      const favss = await response.json();
+      setFavs(favss);
+      console.log(favs);
+    } catch (error) {
+      console.error("There was a problem with the fetch operation: ", error);
+    }
+  };
+
+  const AddFavorite = async (data) => {
+    console.log("add data", data);
+    try {
+      const response = await fetch("http://localhost:8082/addFavorite", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          id: data.id,
+          location: data.location,
+          cost: data.cost,
+          duration: data.duration,
+          url: data.url,
+        }),
+      });
+
+      const result = await response.json();
+      console.log("Inserted document ID:", result);
+      await fetchFavs();
+      setFavsView(0);
+    } catch (error) {
+      console.error("Failed to add the product: ", error);
+    }
+  };
+
+  const removeFavorite = async (id) => {
+    console.log("id", id);
+    try {
+      const response = await fetch(
+        `http://localhost:8082/deleteFavorite/${id}`,
+        {
+          method: "DELETE",
+          headers: { "content-type": "application/json" },
+          body: JSON.stringify({ id: id }),
+        }
+      );
+      const result = await response.json();
+      await fetchFavs();
+      setFavsView(0);
+    } catch (error) {
+      console.error("Error ", error);
+    }
+  };
+
+  const listAllFavorites = (favs) => {
+    return (
+      <div>
+        <div class="container" style={{ margin: 40 }}>
+          <h3>Favorites</h3>
+          <button onClick={AddFavorite}>Add</button>
+          <div class="row">
+            {favs.map((item, index) => (
+              <div class="col" key={index}>
+                <div class="col">
+                  <div
+                    class="card shadow-sm"
+                    style={{ width: 250, margin: 10 }}
+                  >
+                    <img class="card-img-top" src={item.url} />
+
+                    <div class="card-body">
+                      <div class="card-text lead">{item.location}</div>
+
+                      <div class="card-text">{item.cost}</div>
+
+                      <div class="card-text">{item.duration}</div>
+
+                      <button
+                        type="button"
+                        class="btn"
+                        style={{
+                          width: 100,
+                          backgroundColor: "bisque",
+                          marginLeft: 110,
+                        }}
+                        onClick={() => removeFavorite(item.id)}
+                      >
+                        Remove fav
+                      </button>
+                      <button
+                        type="button"
+                        class="btn"
+                        style={{
+                          width: 100,
+                          backgroundColor: "bisque",
+                          marginLeft: 110,
+                        }}
+                        onClick={() => AddFavorite(item)}
+                      >
+                        Add fav
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return <div>{listAllFavorites(favs)}</div>;
 }
 
 export default Favorites;
